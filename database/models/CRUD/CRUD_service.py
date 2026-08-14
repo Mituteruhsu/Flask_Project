ece5bb9e60268e554eda88916365873eb60cc893
Mituteruhsu <104893491+Mituteruhsu@users.noreply.github.com>
2026-08-14 22:02:46 +0800

    Add database/models/CRUD CRUD_service.py services.py

---
+++ database/models/CRUD/CRUD_service.py
@@ -0,0 +1,50 @@
+# database/models/CRUD/CRUD_service.py
+from core.database import db
+
+class CRUDService:
+    def __init__(self, model):
+        self.model = model  # 綁定具體的 db.Model
+
+    # --- Create ---
+    def create(self, **kwargs):
+        """新增一筆紀錄"""
+        instance = self.model(**kwargs)
+        db.session.add(instance)
+        db.session.commit()
+        return instance
+
+    # --- Read ---
+    def get_by_id(self, record_id):
+        """根據 ID 查詢單筆"""
+        # SQLAlchemy 2.0+ 建議用法：db.session.get()
+        return db.session.get(self.model, record_id)
+
+    def get_all(self):
+        """查詢所有紀錄"""
+        return db.session.scalars(db.select(self.model)).all()
+
+    def filter_by(self, **kwargs):
+        """條件查詢（多筆）"""
+        return db.session.scalars(db.select(self.model).filter_by(**kwargs)).all()
+
+    # --- Update ---
+    def update(self, record_id, **kwargs):
+        """根據 ID 更新紀錄"""
+        instance = self.get_by_id(record_id)
+        if not instance:
+            return None
+        for key, value in kwargs.items():
+            if hasattr(instance, key):
+                setattr(instance, key, value)
+        db.session.commit()
+        return instance
+
+    # --- Delete ---
+    def delete(self, record_id):
+        """根據 ID 刪除紀錄"""
+        instance = self.get_by_id(record_id)
+        if not instance:
+            return False
+        db.session.delete(instance)
+        db.session.commit()
+        return True
---
+++ database/models/CRUD/services.py
@@ -0,0 +1,47 @@
+from CRUD_service import CRUDService
+from database.models.user import User
+from database.models.RBAC.role import Role
+from database.models.RBAC.permission import Permission
+from database.models.RBAC.capability import Capability
+from database.models.family.family_member import FamilyMember, FamilyRole
+from database.models.family.family import Family
+
+# 使用者專用 Service，直接繼承所有 CRUD 功力
+class UserService(CRUDService):
+    def __init__(self):
+        super().__init__(User)
+
+    # 如果需要特殊查詢（例如用 email 登入），再額外擴充專屬於 User 的方法
+    def get_by_email(self, email):
+        from database import db
+        return db.session.scalar(db.select(User).filter_by(email=email))
+
+# Role 專用 Service
+class RoleService(CRUDService):
+    def __init__(self):
+        super().__init__(Role)
+
+# Permission 專用 Service
+class PermissionService(CRUDService):
+    def __init__(self):
+        super().__init__(Permission)
+
+# Capability 專用 Service
+class CapabilityService(CRUDService):   
+    def __init__(self):
+        super().__init__(Capability)
+
+# FamilyMember 專用 Service
+class FamilyMemberService(CRUDService):
+    def __init__(self):
+        super().__init__(FamilyMember)
+
+# FamilyRole 專用 Service
+class FamilyRoleService(CRUDService):
+    def __init__(self):
+        super().__init__(FamilyRole)
+
+# Family 專用 Service
+class FamilyService(CRUDService):
+    def __init__(self):
+        super().__init__(Family)
